2ebab81721f6251f6bb6345067187aed89fa1f9b
agreian <jeremy.babouche@gmail.com>
2018-11-02 23:39:41 +0100

    Ajoutez des fichiers projet.

---
+++ M3U Editor/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace M3U_Editor
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
